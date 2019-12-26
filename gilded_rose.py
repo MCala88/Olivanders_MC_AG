4c17ec24841096c8953066abcd863aa39a03d7fb
Miquel Calafell <56043308+MCala88@users.noreply.github.com>
2019-12-26 12:57:32 +0100

    Add files via upload

---
+++ gilded_rose.py
@@ -0,0 +1,67 @@
+class GildedRose(object):
+
+    def __init__(self, items):
+        self.items = items
+        
+    def get_items(self):
+        pass
+
+    def update_quality(self):
+        for item in self.items:
+            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
+                if item.quality > 0:
+                    if item.name != "Sulfuras, Hand of Ragnaros":
+                        item.quality = item.quality - 1
+            else:
+                if item.quality < 50:
+                    item.quality = item.quality + 1
+                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
+                        if item.sell_in < 11:
+                            if item.quality < 50:
+                                item.quality = item.quality + 1
+                        if item.sell_in < 6:
+                            if item.quality < 50:
+                                item.quality = item.quality + 1
+            if item.name != "Sulfuras, Hand of Ragnaros":
+                item.sell_in = item.sell_in - 1
+            if item.sell_in < 0:
+                if item.name != "Aged Brie":
+                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
+                        if item.quality > 0:
+                            if item.name != "Sulfuras, Hand of Ragnaros":
+                                item.quality = item.quality - 1
+                    else:
+                        item.quality = item.quality - item.quality
+                else:
+                    if item.quality < 50:
+                        item.quality = item.quality + 1
+
+
+class Item:
+    def __init__(self, name, sell_in, quality):
+        self.name = name
+        self.sell_in = sell_in
+        self.quality = quality
+
+    def __repr__(self):
+        return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+    
+    class NormalItem:
+            def set_SellIn(self):
+                pass
+            def update_quality(self):
+                pass
+class AgedBrie:
+    def update_quality(self):
+        pass
+class Sulfuras:
+    def update_quality(self):
+        pass
+    def set_SellIn(self):
+        pass
+class Backstagepass:
+    def update_quality(self):
+        pass
+class Conjured:
+    def update_quality(self):
+        pass
