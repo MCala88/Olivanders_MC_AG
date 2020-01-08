d702a2217d622c9108a0d6e4f155f795a7ac23c3
Alexgb24 <56043310+Alexgb24@users.noreply.github.com>
2020-01-08 14:09:37 +0100

    gilded_rose refactor, routes correction

---
+++ gilded_rose.py
@@ -0,0 +1,136 @@
+class GildedRose(object):
+
+    def __init__(self, items):
+        self.items = items
+        
+    def get_items(self, name, sell_in, quality):
+        items = [
+        ['Aged Brie', 2, 0],
+        ['Elixir of the Mongoose', 5, 7],
+        ['Sulfuras, Hand of Ragnaros', 0, 80],
+        ['Backstage passes to a TAFKAL80ETC concert', 15, 20],
+        ['Conjured Mana Cake', 3, 6]
+        ]
+        return items
+        
+    def update_quality(self):
+        for item in self.items:
+            item.update_quality()
+
+class Item:
+    def __init__(self, name, sell_in, quality):
+        self.name = name
+        self.sell_in = sell_in
+        self.quality = quality
+
+    def __repr__(self):
+        return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+    
+class NormalItem(Item):
+    def __init__(self, name, sell_in, quality):
+        Item.__init__(self, name, sell_in, quality)    
+
+    def set_SellIn(self):
+        if self.sell_in:
+            self.sell_in += -1
+
+    def setQuality(self, valor):
+        if self.quality + valor > 50:
+            self.quality = 50
+        elif self.quality + valor >= 0:
+            self.quality = self.quality + valor
+        else:
+            self.quality = 0
+
+    def update_quality(self):
+        if self.sell_in <= 0:
+            self.setQuality(-2)
+        else:
+            self.setQuality(-1)
+        self.set_SellIn()
+
+class AgedBrie(NormalItem):
+    def __init__(self, name, sell_in, quality):
+        Item.__init__(self, name, sell_in, quality)
+
+    def setQuality(self):
+        self.quality()
+
+    def update_quality(self):
+        if self.sell_in > 0:
+            self.quality += 1
+        else:
+            self.quality += 2
+        self.set_SellIn()
+
+class Sulfuras(NormalItem):
+    #def __init__(self, name, sell_in, quality):
+    #    Item.__init__(self, name, sell_in, quality)
+
+    def update_quality(self):
+        assert self.quality == 80      
+
+class Backstagepass(NormalItem):
+    def update_quality(self):
+        if self.sell_in > 10:
+            self.quality += 1
+        elif self.sell_in > 5:
+            self.quality += 2
+        elif self.sell_in > 0:
+            self.quality += 3
+        else:
+            self.quality = 0
+        self.set_SellIn()
+        
+class Conjured(NormalItem):
+    def update_quality(self):
+        if self.sell_in <= 0:
+            self.setQuality(-4)
+        else:
+            self.setQuality(-2)
+        self.set_SellIn()
+
+if __name__ == '__main__':
+
+    item = Sulfuras("Sulfuras, Hand of Ragnaros", 0, 80)
+    print(item)
+
+    for dia in range(1, 30):
+        item.update_quality()
+        print(item)
+    
+    print ('---------------------------------')
+
+    item = NormalItem("Elixir of the Mongoose", 5, 7)
+    print(item)
+
+    for dia in range(1, 30):
+        item.update_quality()
+        print(item)
+
+    print ('---------------------------------')
+
+    item = Backstagepass("Backstage passes to a TAFKAL80ETC concert", 15, 22)
+    print(item)
+
+    for dia in range(1, 30):
+        item.update_quality()
+        print(item)
+
+    print ('---------------------------------')
+
+    item = AgedBrie("Aged Brie", 2, 0)
+    print(item)
+
+    for dia in range(1, 30):
+        item.update_quality()
+        print(item)
+
+    print ('---------------------------------')
+
+    item = Conjured("Conjured Mana Cake", 3, 6)
+    print(item)
+
+    for dia in range(1, 30):
+        item.update_quality()
+        print(item)
